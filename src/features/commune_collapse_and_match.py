3f507920718e2454f6c1091ba7b99b8e6f73a478
Gregoire Lurton <grlurton@ihme.washington.edu>
2016-07-29 11:09:31 -0700

    Adding gps_ids and gps_name in features

--- src/features/commune_collapse_and_match.py
+++ src/features/commune_collapse_and_match.py
@@ -19,11 +19,6 @@ data_electeurs = pd.read_csv('data/processed/voters_list.csv'  , encoding = "ISO
 ## Droping data for electors from the Diaspora
 data_electeurs = data_electeurs[~(data_electeurs['NOM_REGION'] == 'DIASPORA')]
 
-
-
-data_electeurs[data_electeurs.NOM_REGION == 'NIAMEY'].head()
-renaloc.head()
-
 ## Compute population in each data source and merge sources
 def sum_population(data):
     return data.population.sum(skipna = True)
@@ -31,16 +26,14 @@ def sum_population(data):
 def len_population(data) :
     return len(data)
 
-pop_commune = renaloc.groupby([ 'region' , 'commune' ]).apply( sum_population ).reset_index()
-vote_commune = data_electeurs.groupby(['NOM_REGION' , 'NOM_COMMUNE']).apply(len_population).reset_index()
+pop_commune = renaloc.groupby([ 'region' , 'gps_ID' , 'gps_name' , 'commune' ]).apply( sum_population ).reset_index()
+vote_commune = data_electeurs.groupby(['NOM_REGION' , 'gps_ID' , 'gps_name' , 'NOM_COMMUNE' ]).apply( len_population ).reset_index( )
 
-pop_commune.columns = vote_commune.columns = ['region' , 'commune' , 'population']
+pop_commune.columns = vote_commune.columns = ['region' ,  'gps_ID' , 'gps_name' ,'commune' , 'population']
 
 merged_data = pd.merge(left = pop_commune , right = vote_commune ,
-                       how = 'inner' , on = ['commune' , 'region'] ,
+                       how = 'inner' , on = ['commune' , 'region'  , 'gps_ID' , 'gps_name'] ,
                        suffixes = ['_census' , '_voting_list'])
-
-
 
 ## Get proportion of population on voting list
 merged_data['prop_inscrits'] = merged_data.population_voting_list / merged_data.population_census
@@ -49,11 +42,11 @@ merged_data['prop_inscrits'] = merged_data.population_voting_list / merged_data.
 def mean_age(data):
     return data.age.mean()
 
-voters_age = data_electeurs.groupby(['NOM_REGION' , 'NOM_COMMUNE']).apply(mean_age).reset_index()
-voters_age.columns = ['region' , 'commune' , 'mean_age']
+voters_age = data_electeurs.groupby(['NOM_REGION' , 'gps_ID' , 'gps_name' , 'NOM_COMMUNE' ]).apply(mean_age).reset_index()
+voters_age.columns = ['region' , 'gps_ID' , 'gps_name' ,'commune' , 'mean_age']
 
 merged_data = pd.merge(left = merged_data , right = voters_age ,
-                       how = 'inner' , on = ['commune' , 'region'] )
+                       how = 'inner' , on = ['commune' , 'region' ,'gps_ID' , 'gps_name'] )
 
 
 ## Get proportion of women in each commune
@@ -61,11 +54,11 @@ def prop_women(data) :
     u = data.femmes.sum() / data.population.sum(skipna = True)
     return u
 
-prop_women = renaloc.groupby(['region' , 'commune']).apply(prop_women).reset_index()
-prop_women.columns = ['region' , 'commune' , 'prop_women']
+prop_women = renaloc.groupby(['region' ,'gps_ID' , 'gps_name', 'commune']).apply(prop_women).reset_index()
+prop_women.columns = ['region','gps_ID' , 'gps_name' , 'commune' , 'prop_women']
 
 merged_data = pd.merge(left = merged_data , right = prop_women ,
-                       how = 'inner' , on = ['commune' , 'region'] )
+                       how = 'inner' , on = ['commune' , 'region','gps_ID' , 'gps_name'] )
 
 
 
