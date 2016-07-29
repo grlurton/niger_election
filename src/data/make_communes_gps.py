3972c5724ee3fac323e38a0a5ef24a92d66ccfac
Gregoire Lurton <grlurton@ihme.washington.edu>
2016-07-28 21:13:48 -0700

    Commune gps : management of pooled communes (cities) + communes with multiple gps entries

--- src/data/make_communes_gps.py
+++ src/data/make_communes_gps.py
@@ -17,26 +17,48 @@ with open('data/dictionnaries/gps_communes_recodes.json') as json_data:
 communes_listing = pd.read_csv('data/processed/org_units_listing.csv' , encoding = "ISO-8859-1")
 communes_listing = communes_listing[~(communes_listing.NOM_REGION == 'DIASPORA')]
 
-## Enlever accents dans json
+## Get accents out
+carto_commune.nom = carto_commune.nom.str.replace('É','E')
+
+## Extra codes for grouped communes
+extracode = {'MARADI':{'name':'MARADI I,II,III' , 'code':'MARADI_VILLE'} ,
+            'TAHOUA':{'name':'TAHOUA I,II' , 'code':'TAHOUA_VILLE'} ,
+            'ZINDER':{'name':'ZINDER I,II,III,IV,V' , 'code':'ZINDER_VILLE'}}
+
+extracode = pd.DataFrame(extracode).T
+
+communes_listing['gps_name'] = communes_listing['gps_ID'] = ''
+
 out = {}
+mem = []
 for i in range(len(communes_listing)) :
     commune = communes_listing.NOM_COMMUNE[i]
     region = communes_listing.NOM_REGION[i]
     commune_id = communes_listing.ID_COMMUNE[i]
     gps = carto_commune[(carto_commune.nom == commune) & (carto_commune.region == region) ]
-    if len(gps) == 1 :
-        entry = {'name' : commune ,
-                'region' : region ,
-                'coordinates' : gps['coordinates'][0]}
-        out[str(commune_id)] = entry
+    if len(gps) >= 1 :
+        for u in list(gps.index)  :
+            entry = {'name' : commune ,
+                    'region' : region ,
+                    'coordinates' : gps.loc[u , 'coordinates']}
+            out[str(commune_id)] = entry
     if len(gps) == 0 :
         if commune in list(correction_dictionnary[region].keys()) :
             corrected_name = correction_dictionnary[region][commune]
-            gps = carto_commune[(carto_commune.nom == corrected_name) & (carto_commune.region == region) ]
-            entry = {'name' : commune ,
-                    'region' : region ,
-                    'coordinates' : gps['coordinates'][0]}
-            out[str(commune_id)] = entry
+            if corrected_name in list(extracode.name) :
+                commune_id = list(extracode.code[extracode.name == corrected_name])[0]
+                commune = corrected_name
+            if (corrected_name in mem) == False :
+                gps = carto_commune[(carto_commune.nom == corrected_name) & (carto_commune.region == region) ]
+                entry = {'name' : commune ,
+                        'region' : region ,
+                        'coordinates' : gps['coordinates'][0]}
+                out[str(commune_id)] = entry
+            mem = mem + [corrected_name]
+    communes_listing.loc[i , 'gps_name'] = commune
+    communes_listing.loc[i , 'gps_ID'] = commune_id
 
 with open('data/processed/communes_gps.json', 'w') as outfile:
     json.dump(out, outfile)
+
+communes_listing.to_csv('data/processed/org_units_listing.csv' , index = False        )
