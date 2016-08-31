539ae7c1da240e628cc0b5af77f40186fe8701f0
Gregoire Lurton <grlurton@ihme.washington.edu>
2016-08-30 19:55:10 -0700

    moving model and cross val into modelling script

--- src/model/modelling.py
+++ src/model/modelling.py
@@ -7,6 +7,9 @@ import pickle
 
 from scipy.interpolate import UnivariateSpline
 
+## Additional analytic tools
+import statsmodels.formula.api as smf
+
 from multiprocessing.pool import ThreadPool
 
 ## Setting working directory
@@ -28,6 +31,9 @@ voters_data = voters_data[(voters_data.age >= age_adulte) & (voters_data.region 
 ## Model data
 model_data = pd.read_csv('data/processed/commune_collapsed_matched.csv' , encoding = "ISO-8859-1")
 
+def get_bootstrap_sample(voters_data):
+    bootstrap_sample = voters_data.sample(n = len(voters_data) , replace = True)
+    return bootstrap_sample
 
 def age_distrib(data) :
     """
@@ -36,8 +42,102 @@ def age_distrib(data) :
     data.age[data.age > 100] = 100
     out =  np.round(data.age).value_counts() / len(data)
     out = out.reset_index()
+
+
     out.columns = ['age' , 'percentage']
     return out
+
+def get_full_model_data(bootstrap_sample , model_data , levels):
+    """
+    Getting the model data
+    """
+    N_Voters = bootstrap_sample.groupby(levels).apply(len).reset_index()
+    full_data = pd.merge(N_Voters , model_data , on = levels , how = 'inner')
+    return full_data
+
+def get_variables_def(results) :
+    """
+    Function that goes through a statsmodel summary and returns list of variables and values to include in prediction
+    """
+    variables = list(results.params.keys())
+    variable_list = {}
+    for var in variables :
+        value = ''
+        out = {'variable':var}
+        var_split = var.split('[')
+        if len(var_split) > 1 :
+            value = var_split[1].replace(']' , '').replace('T.' , '')
+            out = {'variable':var_split[0] , 'value':value , 'parametre':var}
+        variable_list[var] = out
+    return variable_list
+
+def pred_random_effect(re_model , test_data , random_effect):
+    """
+    Function that returns the response of a statsmodel random effect linear model
+    """
+    test_data['Intercept'] = test_data['Intercept RE'] = 1
+    params = re_model.params
+    variables = get_variables_def(re_model)
+    out = 0
+    for var in list(variables.keys()) :
+        v = variables[var]
+        if len(v) > 1 :
+            out = out + params[v['parametre']]*(str(test_data[v['variable']]) == v['value'])
+        if len(v) == 1 :
+            out = out + params[v['variable']]*test_data[v['variable']]
+
+    if len(test_data[random_effect].unique()) > len(results.random_effects.Intercept) :
+        missing = test_data.loc[(test_data[random_effect].isin(list(results.random_effects.Intercept.index)) == False) , random_effect].unique()
+        for reg in missing :
+            print(missing + ' is missing')
+            re_model.random_effects.Intercept[reg] = 0
+
+    random_effects = list(re_model.random_effects.Intercept[test_data.loc[: , random_effect]])
+    out =  out + random_effects
+
+    return out
+
+def k_fold_validation(n_folds , data , model , random_effect):
+    """
+    Function that performs k_fold validation computations for random effect
+    """
+    samp = np.random.choice(len(data), len(data) , replace = False)
+    test_out = ''
+    for i in range(1,(n_folds + 1)):
+        out = samp[((i- 1)*(len(data)/n_folds)):((i)*(len(data)/n_folds))]
+        train_dat = data[~data.index.isin(out)]
+        test_dat = data[data.index.isin(out)]
+        results = smf.mixedlm(model , data = train_dat , groups = train_dat[random_effect]).fit()
+        test_dat['prediction'] = pred_random_effect(results , test_dat , random_effect)
+        if len(test_out) > 0 :
+            test_out = test_out.append(test_dat)
+        elif len(test_out) == 0 :
+            test_out = test_dat
+    return test_out
+
+
+## Dans ce script on s'arrete a la prediction, et on output le full dataset avec les predictions. On fera les rmse et autres visualisations dans le notebook.
+
+#def rmse(data , predicted):
+    #return np.sqrt(np.average(((data - predicted) * (data - predicted))))
+#    return np.median(np.abs(data - predicted) / data)
+
+
+model_data.columns = list(model_data.columns.str.replace(' ' , '_'))
+
+rmses = []
+n_folds = 10
+final_test = k_fold_validation(n_folds , model_data , 'population_census ~ population_voting_list + mean_age + voting + urbain + prop_women + Ibrahim_Yacouba' , 'region')
+
+rmse(final_test.population_census , final_test.prediction)
+
+
+
+
+
+#######################
+### Splining functions
+#######################
 
 def spl_age(data):
     """
@@ -56,21 +156,16 @@ def impute_non_adulte(splines , age_adulte = age_adulte):
         'extrapol':list(splines(age_extrap))}
     return out
 
-def get_spline_from_sample(voters_data):
+def get_spline_from_sample(sample):
     """
     Wrapper function to get age distribution, spline it and impute non adults from a given sample
     """
-    sample = voters_data.sample(n = len(voters_data) , replace = True)
     age_dist = age_distrib(sample)
     splines = spl_age(age_dist)
     extrapolated_data = impute_non_adulte(splines)
     return extrapolated_data
 
-def get_full_model_data(bootstrap_sample , model_data , levels):
-    """
-    Getting the model data
-    """
-    imputed_voters = pd.merge(bootstrap_sample , N_Voters , on = levels , how = 'inner')
+
 
 
 
