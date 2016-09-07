d5891b14a5c3eb3fddb428b45d1d01da6780db08
Gregoire Lurton <grlurton@ihme.washington.edu>
2016-09-06 23:15:41 -0700

    dropping doublon voters

--- src/data/export_working_datasets.py
+++ src/data/export_working_datasets.py
@@ -20,7 +20,17 @@ locality_data.to_csv('../../data/processed/renaloc_localities.csv' , index = Fal
 voters_data = pd.read_csv('../../data/processed/voters_list.csv' , encoding = "ISO-8859-1")
 len(voters_data)
 
-def keep_unique_voters(data):
-    return data.iloc[0]
+n_ids = voters_data['unique_ID'].value_counts()
+doublons = n_ids[n_ids > 1]
 
-d = voters_data.groupby('unique_ID').apply(keep_unique_voters)
+doub_data = voters_data[voters_data.unique_ID.isin(list(doublons.keys()))]
+unique_data = voters_data[~(voters_data.unique_ID.isin(list(doublons.keys())))]
+
+d = doub_data.groupby('unique_ID').apply(keep_unique_voters)
+
+dedoubled = d.reset_index(drop = True)
+
+final = unique_data.append(dedoubled)
+final = final.reset_index(drop = True)
+
+final.to_csv('../../data/processed/voters_list.csv' , index = False)
