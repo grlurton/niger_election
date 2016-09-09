cbeb0a02f2c815864033c33984fa286fce58ef57
Gregoire Lurton <grlurton@ihme.washington.edu>
2016-09-08 23:33:26 -0700

    correct merging of full data on departement

--- src/data/load_format_renaloc.py
+++ src/data/load_format_renaloc.py
@@ -244,7 +244,7 @@ renaloc.loc[((renaloc['departement'] == 'BIRNI') & (renaloc['region'] == 'TAHOUA
 communes_listing = pd.read_csv('../../data/processed/org_units_listing.csv' , encoding = "ISO-8859-1")
 
 renaloc_full = pd.merge(renaloc , communes_listing ,
-                            on = ['region' , 'commune'] ,
+                            on = ['region' , 'departement' , 'commune'] ,
                             how = 'left')
 
 ## Outputting the full data
