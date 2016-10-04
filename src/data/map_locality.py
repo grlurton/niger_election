368132f94a81f607979acb8b8fd5533eb38ae2aa
Grégoire Lurton <gregoire.lurton@gmail.com>
2016-10-03 22:46:37 -0700

    including dictionnaries

--- src/data/map_locality.py
+++ src/data/map_locality.py
@@ -4,11 +4,13 @@ import os as os
 
 renaloc = pd.read_csv('../../data/processed/renaloc_geolocalized.csv' , encoding = "ISO-8859-1" )
 voting_list = pd.read_csv('../../data/processed/voters_list.csv' ,  encoding = "ISO-8859-1" )
+dico_data = pd.read_csv('../../data/dictionnaries/locality_name_map.csv')
 
+
+## Drop bureaux of the diaspora
 voting_list = voting_list[voting_list.commune_ID < 90000]
 
-voting_list.head()
-
+## making bureaux size => deserves to be in another script
 def get_bureaux_size(data):
     name = data.bureau.iloc[0]
     pop = len(data)
@@ -21,27 +23,59 @@ voting_centers = voting_list.groupby('bureau_ID').apply(get_bureaux_size)
 voting_centers = voting_centers.reset_index()
 voting_centers = voting_centers[['bureau_ID' , 'commune_ID' , 'bureau' , 'N_voters']]
 
+## Make RENALOC ID for each locality
 renaloc['renaloc_ID'] = range(len(renaloc))
 
-voting_centers.bureau = voting_centers.bureau.str.strip().str.lower()
-renaloc.locality = renaloc.locality.str.strip().str.lower()
+##  Make variables on which to match
+voting_centers['bureau_to_match'] = voting_centers.bureau.str.strip().str.lower()
+renaloc['locality_to_match'] = renaloc.locality.str.strip().str.lower()
 
-voting_centers.bureau = voting_centers.bureau.str.replace('ecole primaire' , '')
-voting_centers.bureau = voting_centers.bureau.str.replace('ecole' , '')
+voting_centers.bureau_to_match = voting_centers.bureau_to_match.str.replace('ecole primaire' , '')
+voting_centers.bureau_to_match = voting_centers.bureau_to_match.str.replace('ecole' , '')
 
-voting_centers.bureau = voting_centers.bureau.str.strip().str.lower()
-renaloc.locality = renaloc.locality.str.strip().str.lower()
+voting_centers.bureau_to_match = voting_centers.bureau_to_match.str.replace('1|2' , '').str.strip()
+voting_centers.loc[voting_centers.bureau_to_match.str[-2:-1] == ' ' ,
+                    'bureau_to_match'] = voting_centers.bureau_to_match[voting_centers.bureau_to_match.str[-2:-1] == ' '].str[:-2]
+voting_centers.bureau_to_match = voting_centers.bureau_to_match.str.replace('-' , ' ').str.strip()
 
+renaloc.loc[renaloc.locality_to_match.str[-2:-1] == ' ' ,
+                    'locality'] = renaloc.locality_to_match[renaloc.locality_to_match.str[-2:-1] == ' '].str[:-2]
+
+renaloc.locality_to_match = renaloc.locality_to_match.str.replace('\\/' , ' ')
+
+voting_centers.bureau_to_match = voting_centers.bureau_to_match.str.strip().str.lower()
+renaloc.locality_to_match = renaloc.locality_to_match.str.strip().str.lower()
+
+## Loading approximate manual matching
+voting_centers = pd.merge(voting_centers , dico_data ,
+                        right_on = 'elec_name' ,
+                        left_on = 'bureau_to_match' ,
+                        how = 'left')
+
+voting_centers.loc[pd.isnull(voting_centers.renaloc_name) , 'renaloc_name'] = voting_centers.bureau_to_match[pd.isnull(voting_centers.renaloc_name)]
 
 exact_match = pd.merge(renaloc , voting_centers ,
-                        left_on = ['commune_ID' , 'locality'] ,
-                        right_on = ['commune_ID' , 'bureau'])
+                        left_on = ['commune_ID' , 'locality_to_match'] ,
+                        right_on = ['commune_ID' , 'renaloc_name'])
 
-duplicate_bureaux  = list(exact_match.bureau_ID.value_counts()[(exact_match.bureau_ID.value_counts() > 1)].index)
-geolocalized_bureaux = exact_match[~(exact_match.commune_ID.isin(duplicate_bureaux))]
+#duplicate_bureaux  = list(exact_match.bureau_ID.value_counts()[(exact_match.bureau_ID.value_counts() > 1)].index)
+geolocalized_bureaux = exact_match#[~(exact_match.commune_ID.isin(duplicate_bureaux))]
 
-geolocalized_bureaux.columns
+print(len(geolocalized_bureaux))
 
+## Look at unmatched bureaux
+u = sorted(voting_centers.bureau_to_match[(~(voting_centers.bureau_ID.isin(geolocalized_bureaux.bureau_ID))) & (voting_centers.commune_ID == 10201)])
+
+len(u)
+
+u
+
+v = sorted(renaloc.locality_to_match[~renaloc.renaloc_ID.isin(geolocalized_bureaux.renaloc_ID)  & (renaloc.commune_ID == 10201)])
+
+len(v)
+v
+
+voting_centers.commune_ID.unique()
 
 ## Mapping sur NOM
 ## Show
