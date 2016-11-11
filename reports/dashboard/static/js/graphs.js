b74d1bb3be3871b1d884159e9b50a6eef07a751c
Grégoire Lurton <gregoire.lurton@gmail.com>
2016-11-10 23:53:50 -0800

    adding clustering + updating some dependencies

--- reports/dashboard/static/js/graphs.js
+++ reports/dashboard/static/js/graphs.js
@@ -2,6 +2,7 @@ queue()
 	.defer(d3.json,"/data")
 	.await(makeGraphs);
 
+
 function makeGraphs(error, recordsJson){
 	var records = recordsJson ;
 
@@ -11,20 +12,25 @@ function makeGraphs(error, recordsJson){
 	var drawMap = function(d){
       map.setView([17.6078, 8.0817], 5);
 			mapLink = "<a href='http://openstreetmap.org'>OpenStreetMap</a>";
+			var markers = L.markerClusterGroup();
 			// OSM Background
 			L.tileLayer(
 				'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 					attribution: '&copy; ' + mapLink + ' Contributors', maxZoom: 15,
 				}).addTo(map);
 			// Adding locations markers
-			var long , lat , i , locality ;
+			var long , lat , i , locality , popup;
+
 			for (i = 0 ; i < records.length ; i++) {
 				long = records[i]['longitude'] ;
 				lat = records[i]['latitude'] ;
-				locality = records[i]['locality']
+
+				popup =  '<b> Locality : </b>'+ records[i].locality +
+									'<br/><b> Population : </b>' + records[i].population ;
 				console.log(locality)
-				L.circleMarker([lat,long]).addTo(map).bindPopup(locality).openPopup() ;
+				markers.addLayer(L.circleMarker([lat,long]).bindPopup(popup).openPopup()) ;
 			}
+			map.addLayer(markers);
   };
 	drawMap();
 };
